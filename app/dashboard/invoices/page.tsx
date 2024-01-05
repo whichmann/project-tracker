ff083ed8b8c084ad0e505dee4d705b78d4c094d1
Karol <karol.wichmann@outlook.com>
2024-01-05 23:41:55 +0100

    Initial commit

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Invoices = () => <p>Invoices Page</p>;
+
+export default Invoices;
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Dashboard = () => <p>Dashboard Page</p>;
+
+export default Dashboard;
