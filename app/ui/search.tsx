a98b6b4143f80ecc410950abea63c4091a35aeed
Karol <karol.wichmann@outlook.com>
2024-01-08 21:58:17 +0100

    add pagination

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -2,23 +2,25 @@
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { usePathname, useRouter, useSearchParams } from 'next/navigation';
-import { ChangeEvent } from 'react';
+import { useDebouncedCallback } from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
   const { replace } = useRouter();
   const pathname = usePathname();
-  const handleSearch = (searchTerm: ChangeEvent<HTMLInputElement>) => {
+
+  const handleSearch = useDebouncedCallback((searchTerm: string) => {
     const params = new URLSearchParams(searchParams);
+    params.set('page', '1');
 
     if (searchParams) {
-      params.set('query', searchTerm.target.value);
+      params.set('query', searchTerm);
     } else {
       params.delete('query');
     }
-    console.log({ p: params.toString() });
     replace(`${pathname}?${params.toString()}`);
-  };
+  }, 300);
+
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
@@ -27,7 +29,9 @@ export default function Search({ placeholder }: { placeholder: string }) {
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
-        onChange={handleSearch}
+        onChange={(e) => {
+          handleSearch(e.target.value);
+        }}
         defaultValue={searchParams.get('query')?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
